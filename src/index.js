bc7add9159236892c4a4671bd445a61cdae86f2c
lou-bou <boussaid.louai25@gmail.com>
2026-03-06 21:54:53 +0100

    Create form and show result in console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./js/retrieval.js";
+import { getWeatherData, parseWeatherData } from "./js/retrieval.js";
+
+const form = document.querySelector("form");
+let location;
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  location = form.search.value;
+
+  form.search.value = ""; // clear it after
+
+  const weatherData = await getWeatherData(location);
+  const parsedWeatherData = parseWeatherData(weatherData);
+
+  console.log(parsedWeatherData);
+});
